a6b6978719a81f1d20a810d59c912a2bd8b2c950
sassy <luxeadd@gmail.com>
2025-07-30 16:41:14 +0900

    調整

--- wp/wp-content/themes/themeName/page.php
+++ wp/wp-content/themes/themeName/page.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
-
-
-
+<main>
+  <div class="siteWrapper">
+    <?php if(is_page('contact')): ?>
+      <?php get_template_part('template-parts/form'); ?>
+    <?php endif ?>
+  </div>
+</main>
 <?php get_footer(); ?>
--- wp/wp-content/themes/themeName/parts/pagination.php
+++ wp/wp-content/themes/themeName/parts/pagination.php
@@ -2,8 +2,10 @@
 $args = array(
   // 'total' => 5,
   'mid_size' => 1,
+  'end_size' => 1,
   'prev_text' => '',
   'next_text' => '',
   'screen_reader_text' => ' ',
 );
 the_posts_pagination($args);
+下

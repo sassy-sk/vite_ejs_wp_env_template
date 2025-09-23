136520c005fefe0a515ea4d4df1ce17fab3b85bd
sassy <luxeadd@gmail.com>
2025-09-23 16:51:38 +0900

    admin-sidebar.php

---
+++ wp/wp-content/themes/themeName/admin-sidebar.php
@@ -0,0 +1,26 @@
+<div class="admin-sidebar">
+  <h1>各種更新ページ</h1>
+  <ul>
+    <li><a href="<?php echo admin_url('post.php?post=5792&action=edit'); ?>">各種更新ページへ</a></li>
+  </ul>
+</div>
+
+<style>
+  .admin-sidebar {
+    padding: 20px;
+  }
+  .admin-sidebar ul {
+    list-style: none;
+    padding: 0;
+  }
+  .admin-sidebar ul li {
+    margin-bottom: 10px;
+  }
+  .admin-sidebar ul li a {
+    text-decoration: none;
+    color: #000;
+  }
+  .admin-sidebar ul li a:hover {
+    text-decoration: underline;
+  }
+</style>

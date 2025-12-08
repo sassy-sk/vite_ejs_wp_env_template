16eeb7a374f0a77856d532bc2dbb82fe24cd856c
sassy <luxeadd@gmail.com>
2025-12-08 09:32:31 +0900

    path修正

--- wp/wp-content/themes/themeName/functions/path.php
+++ wp/wp-content/themes/themeName/functions/path.php
@@ -4,9 +4,6 @@
  * 各種Path設定
  */
 
-//localhostの場合のみ開発モードを有効にする
-define('WORDPRESS_DEV', in_array($_SERVER['HTTP_HOST'], ['localhost', '127.0.0.1', 'localhost:8080', 'localhost:3000']) || strpos($_SERVER['HTTP_HOST'], 'localhost:') === 0);
-
 // 画像path設定
 function my_image_path()
 {
